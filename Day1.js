1422aab7b834859c7f5f7f0a742561380ece5b01
Fredrik Svensson <svensson.jf@gmail.com>
2025-12-02 14:38:15 +0100

    Implement remove L and create negative number

--- Day1.js
+++ Day1.js
@@ -1,4 +1,18 @@
 import fs from 'fs';
 
 let fileContent = fs.readFileSync('input.txt', 'utf-8');
-console.log(fileContent);
+let splitFileContent = fileContent.split('\n');
+
+let movement = [];
+
+for (let i = 0; i < splitFileContent.length; i++) {
+  let currentLine = splitFileContent[i];
+  let lengthOfLine = currentLine.length;
+  if (currentLine.startsWith('L')) {
+    currentLine = currentLine.slice(1, lengthOfLine);
+    currentLine = -parseInt(currentLine);
+  }
+  console.log(currentLine);
+}
+
+// let sequence = [];
