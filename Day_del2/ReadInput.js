0227df3a815864f931887545ee69244522b7aa36
Fredrik Svensson <svensson.jf@gmail.com>
2025-12-06 12:34:27 +0100

    Add ReadInput function to read and parse movement instructions from a file

---
+++ Day_del2/ReadInput.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import CheckDirection from './CheckDirection.js';
+
+function ReadInput(inputFile = 'input.txt') {
+    let fileContent = fs.readFileSync(inputFile, 'utf-8');
+    let splitFileContent = fileContent.split('\n');
+    let movements = [];
+
+    for (let i = 0; i < splitFileContent.length; i++) {
+    let currentLine = splitFileContent[i];
+    currentLine = CheckDirection(currentLine);
+    movements.push(currentLine);
+    }
+
+    return movements;
+}
+
+export default ReadInput;
