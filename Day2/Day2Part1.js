2f4ff210b16c5a4f7d49825bc994600439bff62c
Fredrik Svensson <svensson.jf@gmail.com>
2025-12-06 14:56:35 +0100

    Implement logic to calculate and log added up IDs from ID ranges - solved!

--- Day2/Day2Part1.js
+++ Day2/Day2Part1.js
@@ -1,5 +1,23 @@
 import ReadInput from "./ReadInput.js";
 
 let IDRanges = ReadInput('./data/input2.txt');
+let addedUpIDs = 0;
 
-console.log(IDRanges);
+for (let i = 0; i < IDRanges.length; i++) {
+    let currentIDRange = IDRanges[i].split('-');
+
+    for (let j = parseInt(currentIDRange[0]); j <= parseInt(currentIDRange[1]); j++) {
+        let currentID = j.toString();
+
+        if (currentID.length % 2 == 0) {
+            let firstHalf = currentID.slice(0, currentID.length/2);
+            let secondHalf = currentID.slice(currentID.length/2);
+
+            if (firstHalf == secondHalf) {
+                addedUpIDs = addedUpIDs + parseInt(firstHalf + secondHalf);
+            }
+        }
+    }
+}
+
+console.log('Added up IDs: ' + addedUpIDs);
