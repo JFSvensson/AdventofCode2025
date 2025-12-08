f8d72eb2856446bbecd51a513f034c3cc205e042
Fredrik Svensson <fs222id@student.lnu.se>
2025-12-08 20:20:25 +0100

    Begin Day3Part1

---
+++ Day3/Day3Part1.js
@@ -0,0 +1,5 @@
+import ReadInput from "./ReadInput.js";
+
+let batteryData = ReadInput('./data/input.txt');
+
+console.log('Battery Data: ' + batteryData);
---
+++ Day3/ReadInput.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+function ReadInput(inputFile = 'input.txt') {
+    let fileContent = fs.readFileSync(inputFile, 'utf-8');
+    let IDRanges = fileContent.split(',');
+    
+    return IDRanges;
+}
+
+export default ReadInput;
