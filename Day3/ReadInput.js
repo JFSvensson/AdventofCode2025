52bd15ac66c8ba30f66c47dd3d7de06596ab530c
Fredrik Svensson <fs222id@student.lnu.se>
2025-12-08 20:22:24 +0100

    Fix ReadInput function to split file content by new lines instead of commas

--- Day3/ReadInput.js
+++ Day3/ReadInput.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 function ReadInput(inputFile = 'input.txt') {
     let fileContent = fs.readFileSync(inputFile, 'utf-8');
-    let IDRanges = fileContent.split(',');
+    let IDRanges = fileContent.split('\n');
     
     return IDRanges;
 }
